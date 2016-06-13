3252554bd286906570f7c994e55ace8dbab9522f
Robert Hickman <you@example.com>
2016-06-13 21:18:33 +0100

    testing filesystem class

--- verson.py
+++ verson.py
@@ -9,7 +9,7 @@ __builtin__.JOURNAL_STEP_FILE  = '.journal_step'
 
 import os.path as p
 import os, shutil, json, errno
-
+import pdb
 
 ############################################################################################
 # Journaling file storage subsystem, only use one instance at any time, not thread safe
@@ -19,16 +19,32 @@ class storage():
     journal = None
     tmp_idx = 0
 
+############################################################################################
+# init
+############################################################################################
     def __init__(self):
         self.j_file = p.join(DATA_DIR, JOURNAL_FILE)
         self.j_step_file = p.join(DATA_DIR, JOURNAL_STEP_FILE)
         try: os.makedirs(p.join(DATA_DIR, TMP_DIR))
         except: pass
 
+############################################################################################
+# write to journal
+############################################################################################
+    def to_journel(self, data):
+        self.journal.write(json.dumps(data) + "\n")
+        self.journal.flush()
+
+############################################################################################
+# Create a new temp file allocation
+############################################################################################
     def new_tmp(self):
         self.tmp_idx += 1
         return p.join(DATA_DIR, TMP_DIR, 'tmp_' + str(self.tmp_idx)) 
 
+############################################################################################
+# Create a new backup file allocation
+############################################################################################
     def new_backup(self, src):
         try:
             backup_num = int(self.file_get_contents(p.join(DATA_DIR, BACKUP_DIR, '.bk_idx')))
@@ -52,6 +68,7 @@ class storage():
         # under normal operation journal is deleted at end of transaction
         # if it does we need to roll back
         if os.path.isfile(self.j_file):  
+            pdb.set_trace()
             self.rollback()
 
         self.journal = open(DATA_DIR + JOURNAL_FILE, 'w')
@@ -60,26 +77,31 @@ class storage():
 # Do journal rollback
 ############################################################################################
     def rollback(self):
+        # Close the journal for writing
+        self.journal.close()
+        self.journal = None
+
+        # Read the journal
         journal = self.file_get_contents(self.j_file)
+
 
         journ_list = []
         with open(self.j_file) as fle:
             for l in fle:
                 journ_list.append(json.loads(l))
 
-        step = 0 # need to check if step file exists
-
-        try: 
-            targ_step = int(self.file_get_contents(self.j_step_file))
+        # If step file exists there has been a failure in the middle of rollback.
+        # Continue from where it failed.
+        try: targ_step = int(self.file_get_contents(self.j_step_file))
         except OSError:
-           targ_step = 1 
+           targ_step = -1 
 
+        step = 0
         for j_itm in reversed(journ_list):
             # step is always recorded at the end of each rollback item, if the step file
             #records '2', that means something failed during step 3 and we need to start there.  
-            if step > targ_step: 
-                print j_itm
 
+            if step > targ_step: 
             # ------------------------------------------------------
                 if j_itm[0] == 'put_contents':
                     src = j_itm[1]
@@ -134,7 +156,7 @@ class storage():
             # during the rollback and we need to pick up later.
             with open(self.j_step_file, 'w') as f: 
                 f.write(str(step))
-                os.fsync(f)
+                f.flush()
             
             step += 1
 
@@ -172,11 +194,9 @@ class storage():
             tmp_path = self.new_tmp()
             shutil.copy(path, tmp_path)
 
-            self.journal.write(json.dumps(['put_contents', path, tmp_path]) + "\n")
-            os.fsync(self.journal)
+            self.to_journel(['put_contents', path, tmp_path])
         else:
-            self.journal.write(json.dumps(['put_contents', path, None]) + "\n")
-            os.fsync(self.journal)
+            self.to_journel(['put_contents', path, None])
 
         with open(path, 'w') as f:
             f.write(data)
@@ -189,14 +209,15 @@ class storage():
         # record where file moved
 
         if os.path.isfile(src):  
-            if os.path.isfile(dst): # if destination file exists, copy it to tmp first
+            # if destination file exists, copy it to tmp first
+            if os.path.isfile(dst):
                 tmp_path = self.new_tmp()
                 shutil.move(dst, tmp_path)
-                self.journal.write(json.dumps(['overwrite_file', src, dst, tmp_path]) + "\n")
-                os.fsync(self.journal)
-            else: # if dst does not exist, just log the move
-                self.journal.write(json.dumps(['move_file', src, dst]) + "\n")
-                os.fsync(self.journal)
+                self.to_journel(['overwrite_file', src, dst, tmp_path])
+
+            # if dst does not exist, just log the move
+            else: 
+                self.to_journel(['move_file', src, dst])
 
         shutil.move(src, dst)
 
@@ -209,23 +230,75 @@ class storage():
         if os.path.isfile(path):  
             tmp_path = self.new_tmp()
 
-            self.journal.write(json.dumps(['delete_file', path, tmp_path]) + "\n")
-            os.fsync(self.journal)
+            self.to_journel(['delete_file', path, tmp_path])
 
             shutil.move(path, tmp_path)
 
         else:
             raise OSError(errno.ENOENT, 'No such file or directory', path)
 
+def remove_dont_care(path):
+    try: os.remove(path)
+    except: pass
 
-s = storage()
-s.begin()
-s.file_put_contents(DATA_DIR + 'hello', 'test content')
-s.move_file(DATA_DIR + 'hello', DATA_DIR + 'hello2')
-#s.file_put_contents(DATA_DIR + 'hello2', 'test content')
-s.file_put_contents(DATA_DIR + 'hello3', 'test content')
-s.delete_file(DATA_DIR + 'hello3')
-s.rollback()
+def empty_dir(path):
+    shutil.rmtree(path)
+    os.mkdirs(path)
+
+def test_storage_put_rb():
+    empty_dir(DATA_DIR)
+
+    s = storage()
+    s.begin()
+    s.file_put_contents(DATA_DIR + 'hello', 'test content')
+    s.rollback()
+
+
+    if(os.path.isfile(DATA_DIR + 'hello')):
+        raise Exception('error, file still exists, put rollback failed')  
+
+    empty_dir(DATA_DIR)
+
+
+def test_storage_move_rb():
+    remove_dont_care(DATA_DIR + 'hello')
+    remove_dont_care(DATA_DIR + JOURNAL_FILE)
+    remove_dont_care(DATA_DIR + JOURNAL_STEP_FILE)
+
+    s = storage()
+    s.begin()
+    s.file_put_contents(DATA_DIR + 'hello', 'test content')
+    s.commit()
+    s.begin()
+    s.move_file(DATA_DIR + 'hello', DATA_DIR + 'hello2')
+    s.rollback()
+
+    if(os.path.isfile(DATA_DIR + 'hello2')):
+        raise Exception('error, file still exists, move rollback failed')  
+
+    remove_dont_care(DATA_DIR + 'hello')
+
+
+def test_storage_delete_rb():
+    s = storage()
+    s.begin()
+    s.file_put_contents(DATA_DIR + 'hello', 'test content')
+    s.commit()
+    s.begin()
+    s.delete_file(DATA_DIR + 'hello')
+    s.rollback()
+
+    if(not os.path.isfile(DATA_DIR + 'hello')):
+        raise Exception('error, file does not exist, delete rollback failed')  
+
+test_storage_put_rb()
+test_storage_move_rb()
+#test_storage_delete_rb()
+
+
+#s.move_file(DATA_DIR + 'hello', DATA_DIR + 'hello2')
+#s.file_put_contents(DATA_DIR + 'hello3', 'test content')
+#s.delete_file(DATA_DIR + 'hello3')
 
 
 
