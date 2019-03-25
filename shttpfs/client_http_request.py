60f1dc9204afc752f91d1908bd689a907c2a7116
Robert Hickman <you@example.com>
2019-03-25 00:06:46 +0000

    major changes to make client pass all data as headers instead of as post data. Use of urllib2 has been replaced with httplib, additional data is now sent as JSON instead of multipart post, file sending to server is streamed directly without multipart encoding due to bad performance of this in flask. Dependency on poster also removed

---
+++ shttpfs/client_http_request.py
@@ -0,0 +1,62 @@
+import httplib, os, json
+
+class client_http_request:
+############################################################################################
+    def __init__(self, server_base_url):
+        "Configure the servers base URL"
+        self.server_base_url = server_base_url
+
+############################################################################################
+    def begin(self, url, body_length, add_headers, content_type):
+        headers = {
+            'Accept-Encoding': 'identity',
+            'Host'           : self.server_base_url,
+            'Content-Type'   : content_type,
+            'Content-length' : body_length,
+            'Connection'     : 'close',
+            'User-Agent'     : 'SHTTPFS' }
+
+        for k, v in add_headers.iteritems(): headers[k] = v
+
+        # ==
+        conn = httplib.HTTPConnection(self.server_base_url)
+        conn.putrequest('POST', '/' + url)
+        for k, v in headers.iteritems(): conn.putheader(k, v)
+        conn.endheaders()
+        return conn
+
+############################################################################################
+    def request(self, url, headers, data = {}, gen = False):
+        jsn = json.dumps(data)
+        conn = self.begin(url, len(jsn), headers, content_type = 'application/json')
+        conn.send(jsn)
+        res = conn.getresponse()
+
+        if gen == False:
+            return res.read(), dict(res.getheaders())
+
+        else:
+            def writer(path):
+                with open(path, 'wb') as f:
+                    while True:
+                        chunk = res.read(1000 * 1000)
+                        if chunk == b'': break
+                        f.write(chunk)
+
+            return writer, dict(res.getheaders())
+
+############################################################################################
+    def send_file(self, url, headers, fle):
+        size = os.stat(fle).st_size
+
+        conn = self.begin(url, size, headers, content_type = 'application/octet-stream')
+
+        with open(fle, 'rb') as f:
+            while True:
+                chunk = f.read(1000 * 1000)
+                if chunk == b'': break
+                conn.send(chunk)
+
+        res = conn.getresponse()
+        return res.read(), dict(res.getheaders())
+
